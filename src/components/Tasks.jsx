8dc240b8249b94e5e42920b8a921e71edb981813
BiVitoria <biancavitoriavieira1@gmail.com>
2025-06-12 18:27:47 -0300

    initial commit

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,35 @@
+import { ChevronRight, TrashIcon } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}){ 
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task){
+        const query = new URLSearchParams();
+        query.set("title", task.title);
+        query.set("description", task.description);
+        navigate(`/task?${query.toString()}`);
+    }
+
+    return (
+    <ul className="space-y-4 p-6 rounded-md bg-slate-200 shadow">
+        {tasks.map((task => 
+        <li key={task.id} className="flex gap-2" >
+            <button onClick={() => onTaskClick(task.id)} 
+                className= {`bg-slate-400 w-full text-left text-white p-2 rounded-md ${task.isComplete && 'line-through' }`}>
+                {task.title}
+            </button>
+            <button onClick={() => onSeeDetailsClick(task)} className='bg-slate-400 p-2 rounded-md text-white'>
+                <ChevronRight/>
+            </button>
+            <button onClick={() => onDeleteTaskClick(task.id)} className='bg-slate-400 p-2 rounded-md text-white'>
+                <TrashIcon/>
+            </button>
+        </li> ))}
+        
+    </ul>
+    );
+}
+
+
+export default Tasks
